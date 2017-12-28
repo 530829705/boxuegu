79af9b56c1e549aedd663905454e8b669330e7bc
yuanmanting <842539314@qq.com>
2017-12-28 11:22:40 +0800

    课程和个人信息模块(4.20)

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/bean/VideoBean.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/bean/VideoBean.java
@@ -1,8 +1,8 @@
 package cn.edu.gdmec.android.boxuegu.bean;
-
-/**
- * Created by student on 17/12/27.
- */
-
 public class VideoBean {
+    public int chapterId;//章节Id
+    public int videoId;//视频Id
+    public String title;//章节标题
+    public String secondTitle;//视频标题
+    public String videoPath;//视频播放地址
 }
